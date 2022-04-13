b932a4c2eb90d25c1db88e04cff581188cf166fa
baojinlong <237664900@qq.com>
2022-04-13 19:51:26 +0800

    bjl second commit

--- src/main/java/com/bjl/pojo.java
+++ src/main/java/com/bjl/pojo.java
@@ -3,5 +3,6 @@ package com.bjl;
 public class pojo {
     public static void main(String[] args) {
         System.out.printf("你好啊", "hello git ");
+        System.out.printf("你好啊", "hello git2 ");
     }
 }
