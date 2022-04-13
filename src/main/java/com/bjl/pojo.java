21fabe24a9ac8f3b3423d100d9f47991aa9962e1
baojinlong <237664900@qq.com>
2022-04-13 20:10:16 +0800

    bjl hot-fix commit 1

--- src/main/java/com/bjl/pojo.java
+++ src/main/java/com/bjl/pojo.java
@@ -4,5 +4,9 @@ public class pojo {
     public static void main(String[] args) {
         System.out.printf("你好啊", "hello git ");
         System.out.printf("你好啊", "hello git2 ");
+        System.out.printf("你好啊", "hello git3 ");
+        System.out.printf("你好啊", "hello git4 ");
+        System.out.printf("你好啊", "hello git5 ");
+        System.out.printf("你好啊", "hello git6 ");
     }
 }
